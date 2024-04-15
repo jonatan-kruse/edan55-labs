29dffa351dff8e853a87eae08de36d237d4332a7
gustav <gustav.lindqvist000@gmail.com>
2024-04-15 14:23:07 +0200

    fast but faulty

--- independentset/src/lib.rs
+++ independentset/src/lib.rs
@@ -1,15 +1,25 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+pub fn calls(){
+    println!("Calls: {}", CALL_COUNT.load(Ordering::SeqCst));
+    CALL_COUNT.fetch_and(0, Ordering::SeqCst);
+}
+
 pub fn parse_input(input: &str) -> (i32, Vec<u128>) {
     let mut data = input.lines();
     let nodes = data.next().unwrap().parse().unwrap();
     let edges = data.map(string_to_edge).collect::<Vec<_>>();
-    (nodes, edges)
+    (nodes, edges)  
 }
 
-fn string_to_edge(s: &str) -> u128 {
+pub fn string_to_edge(s: &str) -> u128 {
     u128::from_str_radix(&s.replace(" ", "").chars().rev().collect::<String>(), 2).unwrap()
 }
 
 pub fn r0(state: u128, edges: &Vec<u128>) -> u32 {
+    CALL_COUNT.fetch_add(1, Ordering::SeqCst);
     if state == 0 {
         return 0;
     }
@@ -33,6 +43,8 @@ pub fn r0(state: u128, edges: &Vec<u128>) -> u32 {
 }
 
 pub fn r1(state: u128, edges: &Vec<u128>) -> u32 {
+    CALL_COUNT.fetch_add(1, Ordering::SeqCst);
+
     if state == 0 {
         return 0;
     }
@@ -66,21 +78,40 @@ pub fn r2(state: u128, edges: &Vec<u128>) -> u32 {
     let mut max = 0;
     let mut max_i = 0;
     for (i, e) in edges.iter().enumerate() {
-        if (1 << i) & state == 0 {
+        let v = 1 << i;
+        if v & state == 0 {
             continue;
         }
         let deg = (state & e).count_ones();
+        if deg == 2 {
+            let u_index = (state & e).trailing_zeros();
+            let u = 1 << u_index;
+            let w_index = 127 - (state & e).leading_zeros();
+            let w = 1 << w_index;
+
+            if edges[u_index as usize] & w != 0 {
+                return 1 + r2(state & !(u + w + v), edges);
+            } else {
+                let mut new_edges = edges.clone();
+                let z_edge = edges[u_index as usize] | edges[w_index as usize];
+                new_edges[u_index as usize] |= z_edge;
+                new_edges[w_index as usize] &= 0;
+                let new_state = state & !(v + w);
+                return 1 + r2(new_state, &new_edges);
+            }
+        }
         if deg == 1 {
-            return 1 + r2((state ^ (1 << i)) & !edges[i], edges);
+            return 1 + r2((state ^ v) & !edges[i], edges);
         }
         if deg == 0 {
-            return 1 + r2(state ^ (1 << i), edges);
+            return 1 + r2(state ^ v, edges);
         }
         if deg > max {
             max = deg;
             max_i = i;
         }
     }
+
     return r2(state ^ (1 << max_i), edges)
         .max(1 + r1((state ^ (1 << max_i)) & !edges[max_i], edges));
 }
--- independentset/src/main.rs
+++ independentset/src/main.rs
@@ -1,6 +1,49 @@
+use std::fs;
+
 use independentset::*;
 fn main() {
-    let input = include_str!("../data/g120.in");
-    let (nodes, edges) = parse_input(input);
-    println!("{}", r1((1 << nodes)-1, &edges))
+    let file_path = format!("./data/g4.in");
+    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let (nodes, edges) = parse_input(&input);
+    println!("{}", r2((1 << nodes) - 1, &edges));
+    r0_test();
+    r1_test();
+}
+
+fn test() {
+    // u_edge = string_to_edge("1010")
+    // let z_edge = edges[u_index as usize] | edges[w_index as usize];
+    // new_edges[u_index as usize] |= z_edge;
+    // new_edges[w_index as usize] &= 0;
+    // let new_state = state & !(v + w);
+    // return 1 + r2(new_state, &new_edges);
+}
+
+fn r0_test() {
+    for i in 3..7 {
+        let file_path = format!("./data/g{}0.in", i);
+        let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+        let (nodes, edges) = parse_input(&input);
+        println!("R0, g{}0.in: {}", i, r0((1 << nodes) - 1, &edges));
+        calls();
+    }
+}
+
+fn r1_test() {
+    for i in 3..13 {
+        let file_path = format!("./data/g{}0.in", i);
+        let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+        let (nodes, edges) = parse_input(&input);
+        println!("R1, g{}0.in: {}", i, r1((1 << nodes) - 1, &edges));
+        calls();
+    }
+}
+
+fn r2_test() {
+    for i in 4..5 {
+        let file_path = format!("./data/g{}0.in", i);
+        let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+        let (nodes, edges) = parse_input(&input);
+        println!("R2, g{}0.in: {}", i, r2((1 << nodes) - 1, &edges));
+    }
 }
