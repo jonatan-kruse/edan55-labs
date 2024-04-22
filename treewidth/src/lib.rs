7288ae5f6a7c651a71d51be6f63f56a6ecf56612
Jonatan Kruse <jonatankruse@hotmail.com>
2024-04-22 15:16:45 +0200

    Try to parse input

--- treewidth/src/lib.rs
+++ treewidth/src/lib.rs
@@ -1,4 +1,8 @@
+use std::borrow::BorrowMut;
+use std::collections::VecDeque;
+use std::hash::Hash;
 use std::{
+    collections::{HashMap, HashSet},
     fs,
     sync::atomic::{AtomicUsize, Ordering},
 };
@@ -10,18 +14,90 @@ pub fn calls() {
     CALL_COUNT.fetch_and(0, Ordering::SeqCst);
 }
 
-pub fn parse_input(input: &str) -> (i32, Vec<u128>) {
-    let mut data = input.lines();
-    let nodes = data.next().unwrap().parse().unwrap();
-    let edges = data.map(string_to_edge).collect::<Vec<_>>();
-    (nodes, edges)
+pub fn parse_graph(input: &str) -> HashMap<usize, HashSet<usize>> {
+    let data = input.lines();
+    let mut nodes = HashMap::new();
+    data.for_each(|line| {
+        if !(line.starts_with('p') || line.starts_with('c')) {
+            parse_graph_line(line, &mut nodes);
+        }
+    });
+    nodes
 }
 
-pub fn string_to_edge(s: &str) -> u128 {
-    u128::from_str_radix(&s.replace(' ', "").chars().rev().collect::<String>(), 2).unwrap()
+fn parse_graph_line(line: &str, nodes: &mut HashMap<usize, HashSet<usize>>) {
+    let (u, v) = line.split_once(' ').unwrap();
+    let u = u.parse().unwrap();
+    let v = v.parse().unwrap();
+    nodes.entry(u).or_insert_with(HashSet::new).insert(v);
+    nodes.entry(v).or_insert_with(HashSet::new).insert(u);
 }
 
-pub fn path_to_edges(path: &str) -> (i32, Vec<u128>) {
+fn parse_tree(input: &str) -> TreeNode {
+    let data = input.lines();
+    let mut nodes = HashMap::new();
+    let mut tree_nodes = data
+        .filter_map(|line| match line.chars().next().unwrap() {
+            'c' | 's' => None,
+            'b' => Some(TreeNode::from_line(line)),
+            _ => {
+                parse_graph_line(line, &mut nodes);
+                None
+            }
+        })
+        .collect::<HashMap<_, _>>();
+    let root = tree_nodes.values().next().unwrap();
+    root.clone()
+}
+
+#[derive(Debug, Clone)]
+pub struct TreeNode {
+    id: usize,
+    v_t: HashSet<usize>,
+    table: HashMap<usize, usize>,
+    children: HashSet<TreeNode>,
+}
+
+impl Hash for TreeNode {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+impl PartialEq for TreeNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for TreeNode {}
+
+impl TreeNode {
+    fn new(v_t: HashSet<usize>, id: usize) -> Self {
+        Self {
+            id,
+            v_t,
+            table: HashMap::new(),
+            children: HashSet::new(),
+        }
+    }
+
+    fn from_line(line: &str) -> (usize, Self) {
+        let mut chars = line.chars().skip(1);
+        let id = chars.next().unwrap().to_digit(10).unwrap() as usize;
+        let v_t = chars
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect::<HashSet<_>>();
+        (id, Self::new(v_t, id))
+    }
+}
+
+pub fn path_to_tree(path: &str) -> TreeNode {
     let input = fs::read_to_string(path).expect("Should have been able to read the file");
-    parse_input(&input)
+    parse_tree(&input)
+}
+
+pub fn path_to_graph(path: &str) -> HashMap<usize, HashSet<usize>> {
+    let input = fs::read_to_string(path).expect("Should have been able to read the file");
+    parse_graph(&input)
 }
