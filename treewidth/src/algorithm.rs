fd488794618340d6b1eabcebf06fb48b2f60b593
gustav <gustav.lindqvist000@gmail.com>
2024-04-25 10:40:55 +0200

    slow and steady

--- treewidth/src/algorithm.rs
+++ treewidth/src/algorithm.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use crate::arena_tree::ArenaTree;
-use crate::transform_input::{Bag, Bitmap, Graph, Score};
+use crate::transform_input::{Bag, Bitmap, GlobalIndex, Graph, Score};
 
 pub fn solve_max_independent_set(graph: Graph, tree: ArenaTree<Bag>) -> Score {
     let mut max = 0;
@@ -15,26 +15,24 @@ pub fn solve_max_independent_set(graph: Graph, tree: ArenaTree<Bag>) -> Score {
 
 pub fn build_table(graph: &Graph, bag: &Bag) -> HashMap<Bitmap, Score> {
     let matrix = local_adj_matrix(graph, bag);
-    (0..(1 << bag.vt.len()))
-        .filter(|U| 0 == matrix.iter().fold(0, |acc, edge| acc + (edge & U)))
-        .map(|U| (U as u64, U.count_ones() as Score))
+    (0 as Bitmap..(1 << bag.vt.len()))
+        .filter(|u| 0 == matrix.iter().fold(0, |acc, edge| acc + (edge & u)))
+        .map(|u| (u as u64, u.count_ones() as Score))
         .collect::<HashMap<Bitmap, Score>>()
 }
 
-pub fn nodes_to_bitmap(nodes: &HashSet<usize>, bag: &Bag) -> usize {
-    let mut edges = 0;
-    for e in nodes {
-        println!("{}", bag.vt.get(e).unwrap());
-        edges |= 1 << bag.vt.get(e).unwrap();
+pub fn globalindexs_to_localbitmap(subset: &HashSet<GlobalIndex>, bag: &Bag) -> Bitmap {
+    let mut bitmap = 0;
+    for e in subset.intersection(&bag.vt.keys().map(|u| *u).collect::<HashSet<GlobalIndex>>()) {
+        bitmap |= 1 << bag.vt.get(e).unwrap();
     }
-
-    edges
+    bitmap
 }
 
-pub fn local_adj_matrix(graph: &Graph, bag: &Bag) -> Vec<usize> {
+pub fn local_adj_matrix(graph: &Graph, bag: &Bag) -> Vec<Bitmap> {
     let mut matrix = vec![0; bag.vt.len()];
     for (global, local) in bag.vt.iter() {
-        matrix[*local as usize] = nodes_to_bitmap(graph.get(global).unwrap(), bag);
+        matrix[*local as usize] = globalindexs_to_localbitmap(graph.get(global).unwrap(), bag);
     }
     matrix
 }
