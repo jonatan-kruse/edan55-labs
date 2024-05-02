e0382f81d4d528f66ec1d1a94e131133dd17a472
gustav <gustav.lindqvist000@gmail.com>
2024-05-02 15:05:16 +0200

    Markov done

--- fedups/src/markov.rs
+++ fedups/src/markov.rs
@@ -7,13 +7,13 @@ pub fn markov_algo(input: Graph) -> String {
     for edge in input.edges{
         data_a[edge.u * input.nodes + edge.v] = edge.puv;
         data_a[edge.v * input.nodes + edge.u] = edge.pvu;
-        data_b[edge.u] += edge.t * edge.puv;
-        data_b[edge.v] += edge.t * edge.pvu; 
+        data_b[edge.u] -= edge.t * edge.puv;
+        data_b[edge.v] -= edge.t * edge.pvu; 
     }
     let a = Matrix::new(input.nodes, input.nodes, data_a);
     let b = Vector::new(data_b);
 
     let x = (a - Matrix::<f32>::identity(input.nodes)).solve(b).unwrap();
-    "\nfed: ".to_owned() + &(-x.iter().skip(input.fed).next().unwrap()).to_string()
-        + "\npost: " + &(-x.iter().skip(input.post).next().unwrap()).to_string()
+    "\nfed: ".to_owned() + &(x.iter().skip(input.fed).next().unwrap()).to_string()
+        + "\npost: " + &(x.iter().skip(input.post).next().unwrap()).to_string()
 }
